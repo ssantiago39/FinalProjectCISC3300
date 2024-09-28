5dd58ba4aa7b2e35865a18f34cc025e01f7d7783
Nathan Hishon <nathan.hishon@gmail.com>
2024-09-28 13:14:14 -0400

    always use envs

--- app/core/setup.php
+++ app/core/setup.php
@@ -11,24 +11,11 @@ require '../app/models/User.php';
 //set up env variables
 $env = parse_ini_file('../.env');
 
-if($_SERVER['SERVER_NAME'] == 'localhost') {
-    /** database config **/
-    define('DBNAME', $env['DBNAME']);
-    define('DBHOST', $env['DBHOST']);
-    define('DBUSER', $env['DBUSER']);
-    define('DBPASS', $env['DBPASS']);
-    define('DBDRIVER', '');
-
-    define('ROOT', 'http://localhost:8888/');
-
-} else {
-    /** database config **/
-    define('DBNAME', 'my_db');
-    define('DBHOST', 'localhost');
-    define('DBUSER', 'root');
-    define('DBPASS', '');
-    define('DBDRIVER', '');
-}
+define('DBNAME', $env['DBNAME']);
+define('DBHOST', $env['DBHOST']);
+define('DBUSER', $env['DBUSER']);
+define('DBPASS', $env['DBPASS']);
+define('DBDRIVER', '');
 
 //set up other configs
 define('DEBUG', true);
